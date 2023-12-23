ed2766b5805e4cec863744a07383b4bb0741557a
Vlad <vladyslav.pasko@gmail.com>
2023-12-23 06:43:46 +0200

    Start classes

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 from collections import UserDict
+
+class Field:
+    def __init__(self, name, phone, email):
+        self.name = name    # Mandatory
+        self.phone = phone  # Optional | multiple
+        self.email = email  # Optional
+
+class Name:
+    pass
+
+class Phone:
+    pass
+
+class Record:
+    pass
+
+class AddressBook(UserDict):
+    pass
